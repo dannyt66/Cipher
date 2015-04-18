b96502123463f74139eb704fdcf878dbe5596c39
Daniel Thorpe <danielthorpe0@gmail.com>
2015-04-18 20:08:26 +0100

    Commented symbol pair class

--- Cipher/symbolPair.cs
+++ Cipher/symbolPair.cs
@@ -8,16 +8,20 @@ namespace Cipher
 {
     public class symbolPair
     {
+        //Set up a letter and a code that can be retrieved and set
         public string Letter { get; set; }
         public string Code { get; set; }
-        
+        //Set up the class, allowing a letter and a code to be passed to it
         public symbolPair(string letter, string code)
         {
+            //Set the letter and the code to be those that were passed to the class
             Letter = letter;
             Code = code;
         }
+        //Create a new ToString() method
         public override string ToString()
         {
+            //Return the pair in the format "Code -> Letter"
             return Code + " -> " + Letter;
         }
     }
